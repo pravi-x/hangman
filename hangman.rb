98b2c65689703b4d0ccf71a64d85f418efa29953
pravi-x <1998.xar@gmail.com>
2024-09-21 20:05:48 +0300

    add logic for save and load. TODO implematation for save-load

--- hangman.rb
+++ hangman.rb
@@ -25,29 +25,51 @@ class Hangman
     end
   end
 
+  def menu_answer
+    loop do
+      puts "Enter your answer:".colorize(color: :blue, background: :white)
+      answer = gets.chomp.to_i
+      return answer if [1, 2].include?(answer)
+    end
+  end
+
   def display_before_move
     puts "\n"
-    puts "REMAINING GUESSES: #{@remaining_guesses}".colorize(color: :blue, background: :white)
-    puts "USED LETTERS: #{@letters_used.join(', ')}".colorize(color: :blue, background: :white)
-    puts "SECRET: #{@mask}".colorize(color: :blue, background: :white)
+    puts "REMAINING GUESSES: #{@remaining_guesses} ".colorize(color: :blue, background: :white)
+    puts "USED LETTERS: #{@letters_used.join(', ')} ".colorize(color: :blue, background: :white)
+    puts "SECRET: #{@mask} ".colorize(color: :blue, background: :white)
+    puts "DO YOU WANT TO SAVE YOUR GAME? (y/n)".colorize(color: :gray, background: :white)
+    answer = gets.chomp.upcase
+    return ["Y", "YES"].include?(answer)
   end
 
   def display_win
-    puts "CONGRATS!! YOU FOUND THE SECRET WORD, WITH #{@remaining_guesses} GUESSES REMAINING!!".colorize(color: :green, background: :white)
-    puts "SECRET: #{@secret_word}".colorize(color: :green, background: :white)
+    puts "CONGRATS!! YOU FOUND THE SECRET WORD, WITH #{@remaining_guesses} GUESSES REMAINING!! ".colorize(color: :white, background: :green)
+    puts "SECRET: #{@secret_word} ".colorize(color: :white, background: :green)
   end
 
   def display_defeat
-    puts "YOU USED ALL THE GUESSES. BETTER LUCK NEXT TIME".colorize(color: :red, background: :white)
-    puts "SECRET WORD: #{@secret_word}".colorize(color: :red, background: :white)
+    puts "YOU USED ALL THE GUESSES. BETTER LUCK NEXT TIME  ".colorize(color: :red, background: :white)
+    puts "SECRET WORD: #{@secret_word}  ".colorize(color: :red, background: :white)
   end
 
   def run
-    puts "LET'S PLAY HANGMAN. YOU HAVE #{@remaining_guesses} GUESSES TO FIND THE SECRET WORD".colorize(color: :blue, background: :white)
-    puts "\t#{@mask}".colorize(color: :blue)
+    puts "LET'S PLAY HANGMAN.".colorize(color: :blue, background: :white)
+    puts " [1] START A NEW GAME ".colorize(color: :blue, background: :white)
+    puts " [2] LOAD AN OLD GAME ".colorize(color: :blue, background: :white)
 
+    case menu_answer
+    when 1 then new_game
+    when 2 then load_game
+    end
+  end
+  
+  def new_game
     loop do
-      display_before_move
+      save = display_before_move
+      if save
+        save_game
+      end
       letter = guess
       if @secret_word.include?(letter)
         update_mask(letter)
@@ -55,7 +77,7 @@ class Hangman
         @letters_used << letter
         @remaining_guesses -= 1
       end
-
+  
       break if end?
     end
   end
@@ -76,6 +98,16 @@ class Hangman
     else
       false
     end
+  end
+
+  def save_game
+    # Implement functionality here
+    puts "Game saved! " # Placeholder implementation
+  end
+
+  def load_game
+    # Implement functionality here
+    puts "Game loaded! " # Placeholder implementation
   end
 end
 
